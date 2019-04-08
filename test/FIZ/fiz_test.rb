480de9f4a3c63078ac3b036ddd10a8e57a872acf
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:17:15 +0000

    Mon Apr 08 14:17:15 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -17,13 +17,13 @@ class ClientTest < Minitest::Test
   end
 
   def test_buzz_number
-    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5)
+    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(5)
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(52)
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(151)
   end
 
   def test_fizzbuzz_number
-    assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(15)
+    assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(15)
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(60)
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(53)
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(353)
@@ -35,23 +35,24 @@ class ClientTest < Minitest::Test
     assert_equal '22', FizzBuzz.new.fizz_buzz(22)
   end
 
-  # def test_fake_deluxe
-  #   assert_equal 'fake deluxe', FizzBuzz.new.fizz_buzz(11)
-  #   assert_equal 'fake deluxe', FizzBuzz.new.fizz_buzz(1111)
-  #   assert_equal 'fake deluxe', FizzBuzz.new.fizz_buzz(77)
-  #   assert_equal '17', FizzBuzz.new.fizz_buzz(17)
-  #   assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(33)
-  #   assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(333)
-  #   assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(777)
-  #   assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(55)
-  #   assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(555)
-  # end
+  def test_fake_deluxe
+    assert_equal '11', FizzBuzz.new.fizz_buzz(11)
+    assert_equal '1111', FizzBuzz.new.fizz_buzz(1111)
+    assert_equal '77', FizzBuzz.new.fizz_buzz(77)
+    assert_equal '17', FizzBuzz.new.fizz_buzz(17)
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(33)
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(333)
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(777)
+    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(55)
+    assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(555)
+  end
 
   def test_new_deluxe
     assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(3)
     assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(5)
   end
 end
+
 
 
 
