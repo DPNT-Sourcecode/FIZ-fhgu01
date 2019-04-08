f1b490d937bd117f0e37a6688606751d64da1612
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:59:12 +0000

    Mon Apr 08 13:59:12 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -31,9 +31,7 @@ class ClientTest < Minitest::Test
   end
 
   def test_deluxe
-    assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33)
-    assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55)
-    assert_equal 'fizz buzz deluxe', FizzBuzz.new.fizz_buzz(555)
+    assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(66)
     assert_equal 'fizz', FizzBuzz.new.fizz_buzz(6)
     assert_equal 'deluxe', FizzBuzz.new.fizz_buzz(22)
   end
@@ -50,6 +48,7 @@ class ClientTest < Minitest::Test
     assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(555)
   end
 end
+
 
 
 
