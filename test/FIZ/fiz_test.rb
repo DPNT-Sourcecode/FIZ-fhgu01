bba4cfa69358fd26b9773ba4bdcab8a9968b22ba
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:57:40 +0000

    Mon Apr 08 10:57:40 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -7,5 +7,17 @@ Logging.logger.root.appenders = Logging.appenders.stdout
 require_solution 'FIZ'
 
 class ClientTest < Minitest::Test
+  def test_normal_number
+    assert_equal 1, FizzBuzz.new.fizz_buzz(1)
+  end
+
+  def test_fizz_number
+    assert_equal "fizz", FizzBuzz.new.fizz_buzz(3)
+  end
+
+  def test_buzz_number
+    assert_equal "buzz", FizzBuzz.new.fizz_buzz(5)
+  end
 
 end
+
