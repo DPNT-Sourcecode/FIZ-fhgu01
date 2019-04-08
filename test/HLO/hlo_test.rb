16ede0acfd7eeebf4ae14a540daa8183e2b8d0a8
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:26:51 +0000

    Mon Apr 08 10:26:51 UTC 2019

---
+++ test/HLO/hlo_test.rb
@@ -0,0 +1,15 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_hello
+    assert_equal "Hello, world!", Hello.new.hello("world"), 'App should say hello to the world'
+  end
+
+end
