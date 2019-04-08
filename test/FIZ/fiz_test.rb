7477282bcf679662d819d7c440b41349fce31100
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:51:39 +0000

    Mon Apr 08 13:51:39 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -31,9 +31,9 @@ class ClientTest < Minitest::Test
   end
 
   def test_deluxe
-    assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33)
-    assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55)
-    assert_equal 'fizz buzz deluxe', FizzBuzz.new.fizz_buzz(555)
+    # assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33)
+    # assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55)
+    # assert_equal 'fizz buzz deluxe', FizzBuzz.new.fizz_buzz(555)
     assert_equal 'fizz', FizzBuzz.new.fizz_buzz(6)
     assert_equal 'deluxe', FizzBuzz.new.fizz_buzz(22)
   end
@@ -43,6 +43,12 @@ class ClientTest < Minitest::Test
     assert_equal 'fake deluxe', FizzBuzz.new.fizz_buzz(1111)
     assert_equal 'fake deluxe', FizzBuzz.new.fizz_buzz(77)
     assert_equal '17', FizzBuzz.new.fizz_buzz(17)
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(33)
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(333)
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(777)
+    # assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(55)
+    # assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(555)
   end
 end
+
 
