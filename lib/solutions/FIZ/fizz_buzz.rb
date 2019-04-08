8afc521f0f1e2c3df2e3375a80fa2782c2e08a3a
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:09:40 +0000

    Mon Apr 08 11:09:40 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,13 +2,14 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return "fizz buzz" if number % 15 == 0
-    return "fizz" if number % 3 == 0
-    return "buzz" if number % 5 == 0
-    return number
+    return 'fizz buzz' if number % 15 == 0
+    return 'fizz' if number % 3 == 0
+    return 'buzz' if number % 5 == 0
+    number.to_s
   end
 
 end
+
 
 
 
--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -8,21 +8,22 @@ require_solution 'FIZ'
 
 class ClientTest < Minitest::Test
   def test_normal_number
-    assert_equal 1, FizzBuzz.new.fizz_buzz(1)
+    assert_equal '1', FizzBuzz.new.fizz_buzz(1)
   end
 
   def test_fizz_number
-    assert_equal "fizz", FizzBuzz.new.fizz_buzz(3)
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3)
   end
 
   def test_buzz_number
-    assert_equal "buzz", FizzBuzz.new.fizz_buzz(5)
+    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5)
   end
 
   def test_fizzbuzz_number
-    assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(15)
+    assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(15)
   end
 end
+
 
 
 
