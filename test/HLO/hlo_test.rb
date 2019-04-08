3d280faa79995a4a305877270078821a2fcaa6c1
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:32:50 +0000

    Mon Apr 08 10:32:50 UTC 2019

--- test/HLO/hlo_test.rb
+++ test/HLO/hlo_test.rb
@@ -8,10 +8,15 @@ require_solution 'HLO'
 
 class ClientTest < Minitest::Test
 
-  def test_hello
-    assert_equal "Hello, World!", Hello.new.hello(name), 'App should say hello to the world'
+  def test_hello_world
+    assert_equal "Hello, World!", Hello.new.hello("World"), 'App should say hello to the world'
+  end
+
+  def test_hello_john
+    assert_equal "Hello, John!", Hello.new.hello("John"), 'App should say hello to John'
   end
 
 end
+
 
 
